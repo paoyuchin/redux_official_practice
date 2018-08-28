658a84419e7d6916128b33c01f369cabb36d98f7
unknown <yuchinpao@lionmail.com>
2018-08-28 16:34:41 +0800

    Tue

--- todos/src/index.js
+++ todos/src/index.js
@@ -1,18 +1,21 @@
+import { applyMiddleware, createStore } from "redux";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import rootReducer from "./reducers/index";
-import { createStore  } from "redux";
 import { Provider } from "react-redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
 // import promise from "redux-promise";
 import createLogger from "redux-logger";
+import logger from "redux-logger";
 
 
-const logger = createLogger();
+// const logger = createLogger();
 const store = createStore(
-  rootReducer, devToolsEnhancer());
+  rootReducer,
+  applyMiddleware(logger)
+);
 
 render(
   <Provider store={store}>
